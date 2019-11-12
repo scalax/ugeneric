70e68411c46050966a727487f4557b0ec7d0c475
djx314 <djx314@sina.cn>
2019-11-12 14:12:18 +0800

    Test case.

--- src/test/scala/TestKCirce.scala
+++ src/test/scala/TestKCirce.scala
@@ -5,12 +5,19 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import io.circe.syntax._
 import org.scalax.kirito.circe.encoder.PropertyApplication
+import shapeless._
 import shapeless.tag._
 
 class TestKCirce extends AnyFunSpec with Matchers {
   trait IntTag
-  val IntTag = apply[IntTag]
-  case class Test04(i1: String, i2: String, i3: Int @@ IntTag, i4: Long)
+  val IntTag = tag[IntTag]
+
+  trait LongTag
+  val LongTag = tag[LongTag]
+
+  case class Test04(i1: String, i2: String, i3: Int @@ IntTag, i4: Long @@ LongTag)
+
+  implicit val longTagEncoder: Encoder[Long@@LongTag] = Encoder.instance(s => Json.fromString("长整型值是：" + String.valueOf(s)))
 
   implicit def proImplicit: PropertyApplication[Int @@ IntTag] = new PropertyApplication[Int @@ IntTag] {
     override def toProperty(name: String, t: Int @@ IntTag): (String, Json) = ("被改过的 key：" + name, Json.fromString("value 是：" + String.valueOf(t)))
@@ -19,7 +26,7 @@ class TestKCirce extends AnyFunSpec with Matchers {
 
   describe("A case class") {
     it("should encode to a json") {
-      val test04 = Test04(i1 = "pro1", i2 = "pro2", i3 = IntTag(3), i4 = 4L)
+      val test04 = Test04(i1 = "pro1", i2 = "pro2", i3 = IntTag(3), i4 = LongTag(4L))
       println(test04.asJson.noSpaces)
     }
   }
