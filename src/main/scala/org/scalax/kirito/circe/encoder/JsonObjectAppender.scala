73579913889fbfc8d3d5206ec631766d25e16c9b
djx314 <djx314@sina.cn>
2019-11-13 13:23:59 +0800

    Change encoder to Anyval.

--- src/main/scala/org/scalax/kirito/circe/encoder/JsonObjectAppender.scala
+++ src/main/scala/org/scalax/kirito/circe/encoder/JsonObjectAppender.scala
@@ -10,11 +10,13 @@ trait JsonObjectAppender[T, II] extends Any {
 
 object JsonObjectAppender {
   implicit final def asunaCirceEncoderImplicit[T](implicit t: ByNameImplicit[Encoder[T]]): Application2[JsonObjectAppender, PropertyTag[T], T, String] =
-    new Application2[JsonObjectAppender, PropertyTag[T], T, String] {
-      override final def application(context: Context2[JsonObjectAppender]): JsonObjectAppender[T, String] = new JsonObjectAppender[T, String] {
-        override final def appendField(tt: T, name: String, m: java.util.LinkedHashMap[String, Json]) = {
-          m.put(name, t.value(tt))
-        }
-      }
-    }
+    new AppenderApplication2(t)
+
+  class JsonObjectAppenderImpl[T](private val en: ByNameImplicit[Encoder[T]]) extends AnyVal with JsonObjectAppender[T, String] {
+    override final def appendField(tt: T, name: String, m: java.util.LinkedHashMap[String, Json]) = m.put(name, en.value(tt))
+  }
+
+  class AppenderApplication2[T](private val en: ByNameImplicit[Encoder[T]]) extends AnyVal with Application2[JsonObjectAppender, PropertyTag[T], T, String] {
+    override final def application(context: Context2[JsonObjectAppender]): JsonObjectAppenderImpl[T] = new JsonObjectAppenderImpl(en)
+  }
 }
