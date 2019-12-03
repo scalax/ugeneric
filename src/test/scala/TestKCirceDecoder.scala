f34b49193f92c12ebb7138cb41fe9dfc7d683564
djx314 <djx314@sina.cn>
2019-12-03 23:43:26 +0800

    Add json decode test case.

--- src/test/scala/TestKCirceDecoder.scala
+++ src/test/scala/TestKCirceDecoder.scala
@@ -1,9 +1,10 @@
 package org.scalax.kirito.circe
 
-import io.circe.{Decoder, Json, JsonObject}
+import io.circe.Decoder
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import io.circe.syntax._
+import org.scalax.kirito.circe.decoder.DecodeCaseClassApplication
 import shapeless._
 import shapeless.tag._
 
@@ -17,22 +18,23 @@ class TestKCirceDecoder extends AnyFunSpec with Matchers {
   val LongTag = tag[LongTag]
 
   case class Test04(i1: String, i2: String, i3: Int @@ IntTag, i4: Long @@ LongTag)
+  case class Test04Compare(i1: String, i2: String, i3: String, `修改过的 key：i4`: Long)
 
-  implicit val longTagDecoder: Decoder[Long @@ LongTag] = Decoder.decodeString.emapTry(s => Try { s.toLong.asInstanceOf[Long @@ LongTag] })
-  implicit val intTagDecoder: Decoder[Int @@ IntTag]    = Decoder.decodeString.emapTry(s => Try { s.toInt.asInstanceOf[Int @@ IntTag] })
-
-  //implicit def proImplicit: PropertyApplication[Int @@ IntTag] =
-  //new PropertyApplication[Int @@ IntTag]((name, t) => ("被改过的 key：" + name, Json.fromString("value 是：" + String.valueOf(t))))
+  implicit val intTagDecoder: Decoder[Int @@ IntTag] = Decoder.decodeString.emapTry(s => Try { s.toInt.asInstanceOf[Int @@ IntTag] })
+  implicit val longTagDecoder: DecodeCaseClassApplication[Long @@ LongTag] = new DecodeCaseClassApplication(
+    (name, h) => h.get("修改过的 key：i4")(Decoder.decodeLong).map(s => s.asInstanceOf[Long @@ LongTag])
+  )
 
   implicit val decoderTest04: Decoder[Test04] = KCirce.decodeCaseClass
 
   describe("A json") {
     it("should decode to a case class") {
       val test04 = Test04(i1 = "pro1", i2 = "pro2", i3 = IntTag(3), i4 = LongTag(4L))
-      val jsonObj =
-        JsonObject.fromIterable(List(("i1", Json.fromString("pro1")), ("i2", Json.fromString("pro2")), ("i3", Json.fromString("3")), ("i4", Json.fromString("4"))))
-      println(Json.fromJsonObject(jsonObj).as(decoderTest04).getOrElse(throw new Exception("decode failed")))
-      Json.fromJsonObject(jsonObj).as(decoderTest04).getOrElse(throw new Exception("decode failed")) shouldEqual test04
+      val jsonObj = {
+        import io.circe.generic.auto._
+        Test04Compare(i1 = "pro1", i2 = "pro2", i3 = "3", `修改过的 key：i4` = 4L).asJson
+      }
+      jsonObj.as(decoderTest04).getOrElse(throw new Exception("decode failed")) shouldEqual test04
     }
   }
 }
