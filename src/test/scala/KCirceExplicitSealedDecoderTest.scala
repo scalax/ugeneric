33de6f3179d3667e1e1998ee133229f4422bdf53
djx314 <djx314@sina.cn>
2019-12-06 16:32:00 +0800

    Add Sealed Trait Test case.

---
+++ src/test/scala/KCirceExplicitSealedDecoderTest.scala
@@ -0,0 +1,64 @@
+package org.scalax.kirito.circe
+
+import io.circe.Decoder
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import io.circe.syntax._
+import org.scalax.kirito.circe.decoder.DecodeSealedApplication
+import org.scalax.kirito.circe.deImpl2.ParentTest03
+
+package deImpl1 {
+  sealed trait ParentTrait1
+  case class Test01(i1: String, i2: Int)    extends ParentTrait1
+  case object Test02                        extends ParentTrait1
+  case class Test03(i1: String, i2: String) extends ParentTrait1
+}
+
+package deImpl2 {
+  sealed trait ParentTrait2
+  case class Test01(i1: String, i2: Int)        extends ParentTrait2
+  case object Test02                            extends ParentTrait2
+  case class ParentTest03(i1: String, i2: Long) extends ParentTrait2
+  case class ParentTest04(i1: String, i2: String)
+}
+
+class KCirceExplicitSealedDecoderTest extends AnyFunSpec with Matchers {
+
+  val decodeParentTrait: Decoder[deImpl2.ParentTrait2] = {
+    implicit val decodeTest01: Decoder[deImpl2.Test01]       = KCirce.decodeCaseClass
+    implicit val decodeTest02: Decoder[deImpl2.Test02.type]  = Decoder.instance(_ => Right(deImpl2.Test02))
+    implicit val decodeTest04: Decoder[deImpl2.ParentTest04] = KCirce.decodeCaseClass
+    implicit val decodeTest03: DecodeSealedApplication[deImpl2.ParentTest03, deImpl2.ParentTrait2] = new DecodeSealedApplication(
+      (name, cursor) => cursor.get("Test03")(decodeTest04).map(s => ParentTest03(s.i1, s.i2.toLong))
+    )
+    KCirce.decodeSealed
+  }
+
+  describe("A json") {
+    it("should decode to a renamed sealed trait") {
+      val test01 = deImpl1.Test01(i1 = "pro1", i2 = 2)
+      val test03 = deImpl1.Test03(i1 = "pro1-test03", i2 = "4")
+
+      val test01Json = {
+        import io.circe.generic.auto._
+        (test01: deImpl1.ParentTrait1).asJson
+      }
+      val test02Json = {
+        import io.circe.generic.auto._
+        (deImpl1.Test02: deImpl1.ParentTrait1).asJson
+      }
+      val test03Json = {
+        import io.circe.generic.auto._
+        (test03: deImpl1.ParentTrait1).asJson
+      }
+
+      val compareTest01 = deImpl2.Test01(i1 = "pro1", i2 = 2)
+      val compareTest02 = deImpl2.Test02
+      val compareTest03 = deImpl2.ParentTest03(i1 = "pro1-test03", i2 = 4)
+
+      test01Json.as(decodeParentTrait).getOrElse(throw new Exception("decode failed")) shouldEqual compareTest01
+      test02Json.as(decodeParentTrait).getOrElse(throw new Exception("decode failed")) shouldEqual compareTest02
+      test03Json.as(decodeParentTrait).getOrElse(throw new Exception("decode failed")) shouldEqual compareTest03
+    }
+  }
+}
---
+++ src/test/scala/KCirceExplicitSealedEncoderTest.scala
@@ -0,0 +1,64 @@
+package org.scalax.kirito.circe
+
+import io.circe.Encoder
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import io.circe.syntax._
+import org.scalax.kirito.circe.enImpl2.ParentTest04
+import org.scalax.kirito.circe.encoder.EncodeSealedApplication
+
+package enImpl1 {
+  sealed trait ParentTrait1
+  case class Test01(i1: String, i2: Int)    extends ParentTrait1
+  case object Test02                        extends ParentTrait1
+  case class Test03(i1: String, i2: String) extends ParentTrait1
+}
+
+package enImpl2 {
+  sealed trait ParentTrait2
+  case class Test01(i1: String, i2: Int)        extends ParentTrait2
+  case object Test02                            extends ParentTrait2
+  case class ParentTest03(i1: String, i2: Long) extends ParentTrait2
+  case class ParentTest04(i1: String, i2: String)
+}
+
+class KCirceExplicitSealedEncoderTest extends AnyFunSpec with Matchers {
+
+  val encodeParentTrait: Encoder[enImpl2.ParentTrait2] = {
+    implicit val encodeTest01: Encoder[enImpl2.Test01]       = KCirce.encodeCaseClass
+    implicit val encodeTest02: Encoder[enImpl2.Test02.type]  = KCirce.encodeCaseObject
+    implicit val encodeTest04: Encoder[enImpl2.ParentTest04] = KCirce.encodeCaseClass
+    implicit val encodeTest03: EncodeSealedApplication[enImpl2.ParentTest03, enImpl2.ParentTrait2] =
+      new EncodeSealedApplication[enImpl2.ParentTest03, enImpl2.ParentTrait2]((name, value) => ("Test03", ParentTest04(value.i1, value.i2.toString).asJson(encodeTest04)))
+    KCirce.encodeSealed
+  }
+
+  describe("A case class") {
+    it("should encode to a json") {
+      val test01: enImpl1.ParentTrait1 = enImpl1.Test01(i1 = "pro1", i2 = 2)
+      val test02: enImpl1.ParentTrait1 = enImpl1.Test02
+      val test03: enImpl1.ParentTrait1 = enImpl1.Test03(i1 = "pro1-test03", i2 = "4")
+
+      val test01Json = {
+        import io.circe.generic.auto._
+        (test01: enImpl1.ParentTrait1).asJson
+      }
+      val test02Json = {
+        import io.circe.generic.auto._
+        (enImpl1.Test02: enImpl1.ParentTrait1).asJson
+      }
+      val test03Json = {
+        import io.circe.generic.auto._
+        (test03: enImpl1.ParentTrait1).asJson
+      }
+
+      val compareTest01: enImpl2.ParentTrait2 = enImpl2.Test01(i1 = "pro1", i2 = 2)
+      val compareTest02: enImpl2.ParentTrait2 = enImpl2.Test02
+      val compareTest03: enImpl2.ParentTrait2 = enImpl2.ParentTest03(i1 = "pro1-test03", i2 = 4)
+
+      (compareTest01: enImpl2.ParentTrait2).asJson(encodeParentTrait) shouldEqual test01Json
+      (compareTest02: enImpl2.ParentTrait2).asJson(encodeParentTrait) shouldEqual test02Json
+      (compareTest03: enImpl2.ParentTrait2).asJson(encodeParentTrait) shouldEqual test03Json
+    }
+  }
+}
