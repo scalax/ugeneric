ac6de27e0a77d7864728f54e10b885257f176eb1
djx314 <djx314@sina.cn>
2020-05-02 23:04:01 +0800

    Slick Rep.

--- modules/slick/src/main/scala/org/scalax/ugeneric/slick/USlick.scala
+++ modules/slick/src/main/scala/org/scalax/ugeneric/slick/USlick.scala
@@ -1,14 +1,37 @@
 package org.scalax.ugeneric.slick
 
-import asuna.TupleTag
-import asuna.macros.multiply.AsunaMultiplyGeneric
-import slick.lifted.{AbstractTable, ShapedValue}
+import asuna.{Application4, TupleTag}
+import asuna.macros.multiply.{AsunaMultiplyGeneric, AsunaMultiplyRepGeneric}
+import asuna.macros.single.{AsunaGetterGeneric, AsunaSetterGeneric}
+import org.scalax.ugeneric.slick.mutiply.{RepContext, RepNode}
+import slick.ast.{Node, ProductNode}
+import slick.lifted.{AbstractTable, FlatShapeLevel, MappedProjection, Shape, ShapedValue}
+import slick.util.ConstArray
+
+import scala.reflect.ClassTag
 
 object USlick {
 
-  def mapWithTable[Rep, Table <: AbstractTable[Data], Data, P <: TupleTag](table: Table)(implicit p: AsunaMultiplyGeneric.Aux[Table, Data, P]): ShapedValue[_, Data] = {
-    11
-    22
+  def mapWithTable[Rep, Table <: AbstractTable[Data], Data, P <: TupleTag, RepType, DataType, EncodeRef, Packed1](
+    table: Table
+  )(
+    implicit p: AsunaMultiplyGeneric.Aux[Table, Data, P],
+    app: Application4[RepNode, P, RepType, DataType, EncodeRef, Packed1],
+    asunaMultiplyRepGeneric: AsunaMultiplyRepGeneric[Table, Data, RepType],
+    asunaGetterGeneric: AsunaGetterGeneric[Data, DataType],
+    asunaSetterGeneric: AsunaSetterGeneric[Data, DataType],
+    i: ClassTag[Data]
+  ): MappedProjection[Data, DataType] = {
+    val repType = app.application(RepContext)
+    val shape = new Shape[FlatShapeLevel, Packed1, DataType, Packed1] { self =>
+      override def pack(value: Packed1): Packed1                                  = value
+      override def packedShape: Shape[FlatShapeLevel, Packed1, DataType, Packed1] = self
+      override def buildParams(extract: Any => DataType): Packed1                 = repType.buildParams(extract)
+      override def encodeRef(value: Packed, path: Node): Any                      = repType.encodeRef(value, path, 1)
+      override def toNode(value: Packed): Node                                    = ProductNode(ConstArray.from(repType.node(value, List.empty)))
+    }
+    val rep = asunaMultiplyRepGeneric.rep(table)
+    ShapedValue(repType.pack(rep), shape).<>(f = asunaSetterGeneric.setter, g = (asunaGetterGeneric.getter _).andThen(Option.apply))
   }
 
 }
--- modules/slick/src/main/scala/org/scalax/ugeneric/slick/mutiply/RepNode.scala
+++ modules/slick/src/main/scala/org/scalax/ugeneric/slick/mutiply/RepNode.scala
@@ -6,8 +6,9 @@ import slick.lifted.{Shape, ShapeLevel}
 
 trait RepNode[RepType, DataType, EncodeRef, Packed] extends Any {
   def buildParams(extract: Any => DataType): Packed
-  def encodeRef(rep: RepType, path: Node, index: Int): (EncodeRef, Int)
-  def node(rep: RepType, l: List[Node]): List[Node]
+  def encodeRef(rep: Packed, path: Node, index: Int): (EncodeRef, Int)
+  def pack(u: RepType): Packed
+  def node(rep: Packed, l: List[Node]): List[Node]
   def fieldPlus(data: DataType, l: List[Any]): List[Any]
   def fieldTail(l: List[Any]): (DataType, List[Any])
 }
@@ -17,12 +18,13 @@ object RepNode {
   implicit def slickRepNodeApplication[F <: ShapeLevel, Rep, Data, Out](
     implicit dd: Shape[F, Rep, Data, Out]
   ): Application4[RepNode, PropertyTag1[Rep, Data], Rep, Data, Any, Out] = new Application4[RepNode, PropertyTag1[Rep, Data], Rep, Data, Any, Out] {
-    override def application(context: Context4[RepNode]): RepNode[Rep, Data, Any, Out] = new RepNode[Rep, Data, Any, Out] {
-      override def node(rep: Rep, l: List[Node]): List[Node]               = dd.toNode(rep) :: l
+    override def application(context: Context4[RepNode]): RepNode[Rep, Data, Any, Out] = new RepNode[Rep, Data, Any, Out] { self =>
+      override def node(rep: Out, l: List[Node]): List[Node]               = dd.packedShape.toNode(rep) :: l
       override def fieldPlus(data: Data, l: List[Any]): List[Any]          = data :: l
       override def fieldTail(l: List[Any]): (Data, List[Any])              = (l.head.asInstanceOf[Data], l.tail)
-      override def encodeRef(rep: Rep, path: Node, index: Int): (Any, Int) = (dd.encodeRef(rep, Select(path, ElementSymbol(index))), index + 1)
-      override def buildParams(extract: Any => Data): Out                  = dd.buildParams(extract)
+      override def encodeRef(rep: Out, path: Node, index: Int): (Any, Int) = (dd.packedShape.encodeRef(rep, Select(path, ElementSymbol(index))), index + 1)
+      override def pack(u: Rep): Out                                       = dd.pack(u)
+      override def buildParams(extract: Any => Data): Out                  = dd.packedShape.buildParams(extract)
     }
   }
 
