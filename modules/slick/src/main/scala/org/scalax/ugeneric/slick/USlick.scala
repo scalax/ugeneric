b86d99f9d4aac2c49a6a94bf03393695a6996e5b
djx314 <djx314@sina.cn>
2020-06-09 12:11:52 +0800

    Code fix.

--- modules/slick/src/main/scala/org/scalax/ugeneric/slick/USlick.scala
+++ modules/slick/src/main/scala/org/scalax/ugeneric/slick/USlick.scala
@@ -19,24 +19,24 @@ object USlick {
     implicit p: ZsgMultiplyGeneric.Aux[Table, Data, P],
     modelGeneric: ZsgGeneric.Aux[Data, HListDataType],
     app: Application6[RepNode, P, HListDataType, RepType, DataType, EncodeRef, Packed1],
-    asunaMultiplyRepGeneric: ZsgMultiplyRepGeneric[Table, Data, RepType],
-    asunaGetterGeneric: ZsgGetterGeneric[Data, DataType],
-    asunaSetterGeneric: ZsgSetterGeneric[Data, DataType],
+    zsgMultiplyRepGeneric: ZsgMultiplyRepGeneric[Table, Data, RepType],
+    zsgGetterGeneric: ZsgGetterGeneric[Data, DataType],
+    zsgSetterGeneric: ZsgSetterGeneric[Data, DataType],
     i: ClassTag[Data]
   ): ProvenShape[Data] = {
     val repType = app.application(RepContext)
     val shape = new Shape[FlatShapeLevel, Packed1, Data, Packed1] { self =>
       override def pack(value: Packed1): Packed1                              = value
       override def packedShape: Shape[FlatShapeLevel, Packed1, Data, Packed1] = self
-      override def buildParams(extract: Any => Data): Packed1                 = repType.buildParams(extract.andThen(asunaGetterGeneric.getter))
+      override def buildParams(extract: Any => Data): Packed1                 = repType.buildParams(extract.andThen(zsgGetterGeneric.getter))
       override def encodeRef(value: Packed, path: Node): Any                  = repType.encodeRef(value, path, 1)
       override def toNode(value: Packed): Node = {
-        def toBase(v: Any)   = new ProductWrapper(repType.fieldPlus(asunaGetterGeneric.getter(v.asInstanceOf[Data]), List.empty).to(IndexedSeq))
-        def toMapped(v: Any) = asunaSetterGeneric.setter(repType.fieldTail(TupleSupport.buildIndexedSeq(v.asInstanceOf[Product]).to(List))._1)
+        def toBase(v: Any)   = new ProductWrapper(repType.fieldPlus(zsgGetterGeneric.getter(v.asInstanceOf[Data]), List.empty).to(IndexedSeq))
+        def toMapped(v: Any) = zsgSetterGeneric.setter(repType.fieldTail(TupleSupport.buildIndexedSeq(v.asInstanceOf[Product]).to(List))._1)
         TypeMapping(ProductNode(ConstArray.from(repType.node(value, List.empty))), MappedScalaType.Mapper(toBase, toMapped, None), i)
       }
     }
-    val rep = asunaMultiplyRepGeneric.rep(table)
+    val rep = zsgMultiplyRepGeneric.rep(table)
     ShapedValue(repType.pack(rep), shape)
   }
 
