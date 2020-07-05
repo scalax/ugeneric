b788b51f223670797594348c3da7a403b4ac1eba
djx314 <djx314@sina.cn>
2020-07-05 12:44:29 +0800

    slick implicit update.

--- modules/slick/src/main/scala/org/scalax/ugeneric/slick/USlick.scala
+++ modules/slick/src/main/scala/org/scalax/ugeneric/slick/USlick.scala
@@ -13,35 +13,31 @@ import scala.reflect.ClassTag
 
 object USlick {
 
-  class ProvenShapeApply[Data] {
-    def mapWithTable[Rep, HListDataType, Table <: AbstractTable[Data], P, RepType, DataType, EncodeRef, Packed1](
-      table: Table
-    )(
-      implicit p: ZsgMultiplyGeneric.Aux[Table, Data, P],
-      modelGeneric: ZsgGeneric.Aux[Data, HListDataType],
-      app: Application6[RepNode, P, HListDataType, RepType, DataType, EncodeRef, Packed1],
-      zsgMultiplyRepGeneric: ZsgMultiplyRepGeneric[Table, Data, RepType],
-      zsgGetterGeneric: ZsgGetterGeneric[Data, DataType],
-      zsgSetterGeneric: ZsgSetterGeneric[Data, DataType],
-      i: ClassTag[Data]
-    ): ProvenShape[Data] = {
-      val repType = app.application
-      val shape = new Shape[FlatShapeLevel, Packed1, Data, Packed1] { self =>
-        override def pack(value: Packed1): Packed1                              = value
-        override def packedShape: Shape[FlatShapeLevel, Packed1, Data, Packed1] = self
-        override def buildParams(extract: Any => Data): Packed1                 = repType.buildParams(extract.andThen(zsgGetterGeneric.getter))
-        override def encodeRef(value: Packed, path: Node): Any                  = repType.encodeRef(value, path, 1)
-        override def toNode(value: Packed): Node = {
-          def toBase(v: Any)   = new ProductWrapper(repType.fieldPlus(zsgGetterGeneric.getter(v.asInstanceOf[Data]), List.empty).to(IndexedSeq))
-          def toMapped(v: Any) = zsgSetterGeneric.setter(repType.fieldTail(TupleSupport.buildIndexedSeq(v.asInstanceOf[Product]).to(List))._1)
-          TypeMapping(ProductNode(ConstArray.from(repType.node(value, List.empty))), MappedScalaType.Mapper(toBase, toMapped, None), i)
-        }
+  def mapWithTable[Data, Rep, HListDataType, Table <: AbstractTable[Data], P, RepType, DataType, EncodeRef, Packed1](
+    table: Table
+  )(
+    implicit p: ZsgMultiplyGeneric.Aux[Table, Data, P],
+    modelGeneric: ZsgGeneric.Aux[Data, HListDataType],
+    app: Application6[RepNode, P, HListDataType, RepType, DataType, EncodeRef, Packed1],
+    zsgMultiplyRepGeneric: ZsgMultiplyRepGeneric[Table, Data, RepType],
+    zsgGetterGeneric: ZsgGetterGeneric[Data, DataType],
+    zsgSetterGeneric: ZsgSetterGeneric[Data, DataType],
+    i: ClassTag[Data]
+  ): ProvenShape[Data] = {
+    val repType = app.application
+    val shape = new Shape[FlatShapeLevel, Packed1, Data, Packed1] { self =>
+      override def pack(value: Packed1): Packed1                              = value
+      override def packedShape: Shape[FlatShapeLevel, Packed1, Data, Packed1] = self
+      override def buildParams(extract: Any => Data): Packed1                 = repType.buildParams(extract.andThen(zsgGetterGeneric.getter))
+      override def encodeRef(value: Packed, path: Node): Any                  = repType.encodeRef(value, path, 1)
+      override def toNode(value: Packed): Node = {
+        def toBase(v: Any)   = new ProductWrapper(repType.fieldPlus(zsgGetterGeneric.getter(v.asInstanceOf[Data]), List.empty).to(IndexedSeq))
+        def toMapped(v: Any) = zsgSetterGeneric.setter(repType.fieldTail(TupleSupport.buildIndexedSeq(v.asInstanceOf[Product]).to(List))._1)
+        TypeMapping(ProductNode(ConstArray.from(repType.node(value, List.empty))), MappedScalaType.Mapper(toBase, toMapped, None), i)
       }
-      val rep = zsgMultiplyRepGeneric.rep(table)
-      ShapedValue(repType.pack(rep), shape)
     }
+    val rep = zsgMultiplyRepGeneric.rep(table)
+    ShapedValue(repType.pack(rep), shape)
   }
-
-  def mapWithTable[Data](n: Context6[RepNode] => ProvenShapeApply[Data] => ProvenShape[Data]) = n(RepContext)(new ProvenShapeApply)
 
 }
